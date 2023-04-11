f20d90c1df74809a6f392b179e05b41b4b17fb37
jnamutebi <jnamutebi@dfcugroup.com>
2023-04-11 23:18:21 +0300

    Add Customer, Loan, User and Log trail models

---
+++ LoanStatusWebAPI/Models/Logtrails.cs
@@ -0,0 +1,12 @@
+﻿namespace LoanStatusWebAPI.Models
+{
+    public class Logtrails
+    {
+        public int Id { get; set; }
+        public string EndPoint { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string ActionbY { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
