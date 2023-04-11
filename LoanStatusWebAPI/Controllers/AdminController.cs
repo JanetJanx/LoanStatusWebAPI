8e12e55fd4e00994a57c5d6cd46ba505e261612d
jnamutebi <jnamutebi@dfcugroup.com>
2023-04-11 23:19:43 +0300

    Added Customer, User and Admin controllers

---
+++ LoanStatusWebAPI/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using LoanStatusWebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LoanStatusWebAPI.Models;
+
+namespace LoanStatusWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly LoanStatusWebAPIContext _context;
+
+        public AdminController(LoanStatusWebAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("statistics")]
+        public IActionResult GetStatistics()
+        {
+            var statistics = new
+            {
+                TotalRequests = _context.Logtrails.Count(),
+                SuccessfulRequests = _context.Logtrails.Where(u => u.Status == "error").Count(),
+                FailedRequests = _context.Logtrails.Where(u => u.Status == "success").Count(),
+                ConflictRequests = _context.Logtrails.Where(u => u.Status == "conflict").Count(),
+            };
+
+            return StatusCode(100, new { status = "success", Message = statistics });
+        }
+    }
+}
