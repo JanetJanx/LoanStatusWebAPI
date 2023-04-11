3227407cea4f785b73a7f9ece052348dd78c5180
jnamutebi <jnamutebi@dfcugroup.com>
2023-04-12 01:31:26 +0300

    Removed obj and bin files. Added minor changes to Admin Int;

--- LoanStatusWebAPI/Controllers/AdminController.cs
+++ LoanStatusWebAPI/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using LoanStatusWebAPI.Models;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LoanStatusWebAPI.Controllers
 {
@@ -16,18 +18,101 @@ namespace LoanStatusWebAPI.Controllers
             _context = context;
         }
 
-        [HttpGet("statistics")]
-        public IActionResult GetStatistics()
+        [HttpPost("statistics")]
+        public IActionResult GetStatistics(DateTime dateTime)
         {
+            // Check if account number is null or empty
+            if (dateTime == null)
+            {
+                LogAuditTrail("Authorised user", "Statistics", "error", "Date cannot be null or empty.");
+                return StatusCode(99, new { status = "error", message = "Date number cannot be null or empty." });
+            }
+
             var statistics = new
             {
-                TotalRequests = _context.Logtrails.Count(),
-                SuccessfulRequests = _context.Logtrails.Where(u => u.Status == "error").Count(),
-                FailedRequests = _context.Logtrails.Where(u => u.Status == "success").Count(),
-                ConflictRequests = _context.Logtrails.Where(u => u.Status == "conflict").Count(),
+                TotalRequests = GetTotalRequests(),
+                SuccessfulRequests = GetSuccessfulRequests(),
+                FailedRequests = GetFailedRequests(),
+                ConflictedRequests = GetConflictedRequests(),
             };
 
             return StatusCode(100, new { status = "success", Message = statistics });
         }
+
+        [NonAction]
+        public int GetTotalRequests()
+        {
+            var setcount = _context.Logtrails.Count();
+            if (setcount >= 0)
+            {
+                return setcount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        [NonAction]
+        public int GetSuccessfulRequests()
+        {
+            var setcount = _context.Logtrails.Where(u => u.Status == "success").Count();
+            if (setcount >= 0)
+            {
+                return setcount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        [NonAction]
+        public int GetFailedRequests()
+        {
+            var setcount = _context.Logtrails.Where(u => u.Status == "error").Count();
+            if (setcount >= 0)
+            {
+                return setcount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        [NonAction]
+        public int GetConflictedRequests()
+        {
+            var setcount = _context.Logtrails.Where(u => u.Status == "conflict").Count();
+            if (setcount >= 0)
+            {
+                return setcount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        [NonAction]
+        public void LogAuditTrail(string username, string endpoint, string status, string message)
+        {
+            try
+            {
+                Logtrails auditTrails = new Logtrails();
+                auditTrails.EndPoint = endpoint;
+                auditTrails.Status = status;
+                auditTrails.DateCreated = DateTime.Now;
+                auditTrails.ActionbY = username;
+                auditTrails.Message = message;
+                _context.Add(auditTrails);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                //do nothing;
+            }
+        }
+
     }
 }
