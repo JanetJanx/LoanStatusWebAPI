2f14ef09c7b2cb64f943772904a7d633875eae2b
jnamutebi <jnamutebi@dfcugroup.com>
2023-04-11 23:26:01 +0300

    Added app settings

---
+++ LoanStatusWebAPI/Data/LoanStatusWebAPIContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoanStatusWebAPI.Models;
+
+namespace LoanStatusWebAPI.Data
+{
+    public class LoanStatusWebAPIContext : DbContext
+    {
+        public LoanStatusWebAPIContext (DbContextOptions<LoanStatusWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoanStatusWebAPI.Models.Customer> Customer { get; set; } = default!;
+
+        public DbSet<LoanStatusWebAPI.Models.Loan>? Loan { get; set; }
+
+        public DbSet<LoanStatusWebAPI.Models.User>? User { get; set; }
+        public DbSet<LoanStatusWebAPI.Models.Logtrails>? Logtrails { get; set; }
+    }
+}
---
+++ LoanStatusWebAPI/Migrations/20230411181958_AddLogsUpdatedONE.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanStatusWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLogsUpdatedONE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logtrails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EndPoint = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ActionbY = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logtrails", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logtrails");
+        }
+    }
+}
